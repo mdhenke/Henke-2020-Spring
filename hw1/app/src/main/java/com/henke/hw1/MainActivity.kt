2762bae05981a8584c0a36f3ad69dc01b720a383
Marcus <mdhenke8108@gmail.com>
2020-01-23 16:54:34 -0700

    finished hw1

--- hw1/app/src/main/java/com/henke/hw1/MainActivity.kt
+++ hw1/app/src/main/java/com/henke/hw1/MainActivity.kt
@@ -3,6 +3,7 @@ package com.henke.hw1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +14,24 @@ class MainActivity : AppCompatActivity() {
         catBtn.setOnClickListener {
             var string:String = editText.text.toString() + editText2.text.toString()
             catBox.text = string
+        }
+
+        d4Btn.setOnClickListener {
+            val rand = Random.nextInt(1, 5) + damageText.text.toString().toInt()
+            var string:String = "You rolled for $rand points of damage!"
+            textView2.text = string
+        }
+
+        d6Btn.setOnClickListener {
+            val rand = Random.nextInt(1, 7) + damageText.text.toString().toInt()
+            var string:String = "You rolled for $rand points of damage!"
+            textView2.text = string
+        }
+
+        d8Btn.setOnClickListener {
+            val rand = Random.nextInt(1, 9) + damageText.text.toString().toInt()
+            var string:String = "You rolled for $rand points of damage!"
+            textView2.text = string
         }
     }
 }
