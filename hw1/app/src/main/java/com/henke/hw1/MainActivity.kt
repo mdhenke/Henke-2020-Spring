f23ca99382f5061017f9123a8e89faa065b0c6eb
Marcus <mdhenke8108@gmail.com>
2020-01-22 21:25:02 -0700

    concat

--- hw1/app/src/main/java/com/henke/hw1/MainActivity.kt
+++ hw1/app/src/main/java/com/henke/hw1/MainActivity.kt
@@ -2,11 +2,17 @@ package com.henke.hw1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        catBtn.setOnClickListener {
+            var string:String = editText.text.toString() + editText.text.toString()
+            catBox.text = string
+        }
     }
 }
